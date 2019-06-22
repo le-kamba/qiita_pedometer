3d9de4818306a0a17807490ae8b78e40bb52b628
神庭幸枝 <le.s.kamba@gmail.com>
2019-06-22 18:10:00 +0900

    MLKitお試し

--- app/src/main/java/jp/les/kasa/sample/mykotlinapp/activity/logitem/LogInputFragment.kt
+++ app/src/main/java/jp/les/kasa/sample/mykotlinapp/activity/logitem/LogInputFragment.kt
@@ -1,13 +1,21 @@
 package jp.les.kasa.sample.mykotlinapp.activity.logitem
 
 
+import android.Manifest.permission
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
 import jp.les.kasa.sample.mykotlinapp.R
 import jp.les.kasa.sample.mykotlinapp.alert.ErrorDialog
 import jp.les.kasa.sample.mykotlinapp.clearTime
@@ -17,9 +25,12 @@ import jp.les.kasa.sample.mykotlinapp.data.WEATHER
 import jp.les.kasa.sample.mykotlinapp.getDateStringYMD
 import kotlinx.android.synthetic.main.fragment_log_input.*
 import kotlinx.android.synthetic.main.fragment_log_input.view.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
+import java.io.File
 import java.util.*
 
-
+@RuntimePermissions()
 class LogInputFragment : Fragment() {
 
     companion object {
@@ -68,6 +79,11 @@ class LogInputFragment : Fragment() {
             DateSelectDialogFragment().show(fgm, DATE_SELECT_TAG)
         }
 
+        // カメラボタン
+        contentView.button_camera.setOnClickListener {
+            onCameraButtonWithPermissionCheck()
+        }
+
         return contentView
     }
 
@@ -96,6 +112,65 @@ class LogInputFragment : Fragment() {
     private fun validation(): Int? {
         val selectDate = viewModel.selectDate.value?.clearTime()
         return logInputValidation(today, selectDate, edit_count.text.toString())
+    }
+
+    @NeedsPermission(permission.READ_EXTERNAL_STORAGE, permission.WRITE_EXTERNAL_STORAGE)
+    internal fun onCameraButton() {
+        // ストレージから固定ファイルを読込
+        val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+//        val file = File(dir, "Camera/IMG_20190622_165554.jpg")
+        val file = File(dir, "Camera/IMG_20190622_180008.jpg")
+//        val file = File(dir, "Camera/IMG_20190622_174454.jpg")
+//        val file = File(dir, "Camera/IMG_20190622_174413.jpg")
+        Log.d("MYOCR", "file=${file.absoluteFile}")
+        if (!file.exists()) return
+
+        val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+//        val detector = FirebaseVision.getInstance()
+//            .onDeviceTextRecognizer
+
+        val detector = FirebaseVision.getInstance().cloudTextRecognizer
+
+        val result = detector.processImage(image)
+            .addOnSuccessListener { firebaseVisionText ->
+                // Task completed successfully
+                processTextBlock(firebaseVisionText)
+            }
+            .addOnFailureListener {
+                // Task failed with an exception
+                // ...
+                Toast.makeText(context, "OCR出来ない！", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun processTextBlock(result: FirebaseVisionText) {
+        val resultText = result.text
+        for (block in result.textBlocks) {
+            val blockText = block.text
+            Log.d("MYOCR", "blockText=$blockText")
+            val blockConfidence = block.confidence
+            val blockLanguages = block.recognizedLanguages
+            val blockCornerPoints = block.cornerPoints
+            val blockFrame = block.boundingBox
+            for (line in block.lines) {
+                val lineText = line.text
+                Log.d("MYOCR", "lineText=$lineText")
+                val lineConfidence = line.confidence
+                val lineLanguages = line.recognizedLanguages
+                val lineCornerPoints = line.cornerPoints
+                val lineFrame = line.boundingBox
+                for (element in line.elements) {
+                    val elementText = element.text
+                    Log.d("MYOCR", "elementText=$elementText")
+                    val elementConfidence = element.confidence
+                    val elementLanguages = element.recognizedLanguages
+                    val elementCornerPoints = element.cornerPoints
+                    val elementFrame = element.boundingBox
+                }
+            }
+        }
     }
 }
 
