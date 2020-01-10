ef86f686ac5bc322a0e99c0dc8c7b7a56a3cb648
les-kasa <3480914+le-kamba@users.noreply.github.com>
2020-01-10 19:02:15 +0900

    viewModelScope

--- app/src/main/java/jp/les/kasa/sample/mykotlinapp/MainViewModel.kt
+++ app/src/main/java/jp/les/kasa/sample/mykotlinapp/MainViewModel.kt
@@ -3,14 +3,12 @@ package jp.les.kasa.sample.mykotlinapp
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import jp.les.kasa.sample.mykotlinapp.data.LogRepository
 import jp.les.kasa.sample.mykotlinapp.data.LogRoomDatabase
 import jp.les.kasa.sample.mykotlinapp.data.StepCountLog
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 /**
  * MainViewModel
@@ -23,30 +21,17 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
     // 全データリスト
     val stepCountList: LiveData<List<StepCountLog>>
 
-    // coroutine用
-    private var parentJob = Job()
-
-    private val coroutineContext: CoroutineContext
-        get() = parentJob + Dispatchers.Main
-
-    private val scope = CoroutineScope(coroutineContext)
-
     init {
         val logDao = LogRoomDatabase.getDatabase(app).logDao()
         repository = LogRepository(logDao)
         stepCountList = repository.allLogs
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        parentJob.cancel()
-    }
-
-    fun addStepCount(stepLog: StepCountLog) = scope.launch(Dispatchers.IO) {
+    fun addStepCount(stepLog: StepCountLog) = viewModelScope.launch(Dispatchers.IO) {
         repository.insert(stepLog)
     }
 
-    fun deleteStepCount(stepLog: StepCountLog) = scope.launch(Dispatchers.IO) {
+    fun deleteStepCount(stepLog: StepCountLog) = viewModelScope.launch(Dispatchers.IO) {
         repository.delete(stepLog)
     }
 }
